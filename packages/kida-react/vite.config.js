b4f4da038c935670e9fb619593799fb2c871bdc4
Dan Onoshko <danon0404@gmail.com>
2025-01-03 19:41:45 +0400

    feat(react): kida integration for react (#45)

---
+++ packages/kida-react/vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    lib: {
+      formats: ['es'],
+      entry: {
+        index: './src/index.tsx'
+      }
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'react/jsx-runtime',
+        'kida'
+      ]
+    },
+    sourcemap: true,
+    minify: false,
+    emptyOutDir: false
+  },
+  test: {
+    environment: 'happy-dom',
+    setupFiles: ['@testing-library/jest-dom/vitest'],
+    exclude: [...configDefaults.exclude, './package'],
+    coverage: {
+      reporter: ['lcovonly', 'text'],
+      include: ['src/**/*']
+    }
+  }
+})
