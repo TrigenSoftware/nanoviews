62b6e822340d721bc7fc5b528a75dfba54c309cf
dangreen <danon0404@gmail.com>
2026-02-09 12:58:33 +0000

    test(benchmarks): add more libraries to compare

--- packages/benchmarks/atom.js
+++ packages/benchmarks/atom.js
@@ -2,6 +2,12 @@ import { Bench } from 'tinybench'
 import * as nanostores from 'nanostores'
 import * as signals from 'alien-signals-1'
 import * as signals3 from 'alien-signals-3'
+import * as rxjs from 'rxjs'
+import * as effector from 'effector'
+import * as mobx from 'mobx'
+import * as valtio from 'valtio'
+import * as svelteStore from 'svelte/store'
+import * as jotai from 'jotai'
 import * as agera from '../agera/dist/index.js' // 'agera'
 
 const bench = new Bench({
@@ -33,6 +39,40 @@ bench
     const $store = agera.mountable(agera.signal(0))
 
     $store($store() + 1)
+  })
+  .add('rxjs / BehaviorSubject', () => {
+    const $store = new rxjs.BehaviorSubject(0)
+
+    $store.next($store.getValue() + 1)
+  })
+  .add('effector / store', () => {
+    const inc = effector.createEvent()
+    const $store = effector.createStore(0).on(inc, s => s + 1)
+
+    inc()
+  })
+  .add('mobx / observable.box', () => {
+    const $store = mobx.observable.box(0)
+
+    $store.set($store.get() + 1)
+  })
+  .add('valtio / proxy', () => {
+    const $store = valtio.proxy({
+      v: 0
+    })
+
+    $store.v += 1
+  })
+  .add('svelte/store / writable', () => {
+    const $store = svelteStore.writable(0)
+
+    $store.update(n => n + 1)
+  })
+  .add('jotai / atom (createStore)', () => {
+    const store = jotai.createStore()
+    const count = jotai.atom(0)
+
+    store.set(count, c => c + 1)
   })
 
 await bench.run()
--- packages/benchmarks/effect.js
+++ packages/benchmarks/effect.js
@@ -1,78 +1,176 @@
+import assert from 'node:assert/strict'
 import { Bench } from 'tinybench'
 import * as nanostores from 'nanostores'
 import * as signals from 'alien-signals-1'
 import * as signals3 from 'alien-signals-3'
+import * as rxjs from 'rxjs'
+import * as effector from 'effector'
+import * as mobx from 'mobx'
+import * as valtio from 'valtio'
+import * as svelteStore from 'svelte/store'
+import * as jotai from 'jotai'
 import * as agera from '../agera/dist/index.js' // 'agera'
 
 const bench = new Bench({
-  time: 1000
+  time: 1000,
+  throws: true
 })
 
 bench
   .add('nanostores / effect', () => {
     const $store = nanostores.atom(0)
     const logs = []
-
-    nanostores.effect([$store], (store) => {
+    const unsub = nanostores.effect([$store], (store) => {
       logs.push(store)
     })
 
     $store.set($store.get() + 1)
     $store.set($store.get() + 1)
 
-    console.assert(logs.length === 3)
+    unsub()
+    assert.equal(logs.length, 3)
   })
   .add('alien-signals-1 / effect', () => {
     const $store = signals.signal(0)
     const logs = []
-
-    signals.effect(() => {
+    const unsub = signals.effect(() => {
       logs.push($store())
     })
 
     $store($store() + 1)
     $store($store() + 1)
 
-    console.assert(logs.length === 3)
+    unsub()
+    assert.equal(logs.length, 3)
   })
   .add('alien-signals-3 / effect', () => {
     const $store = signals3.signal(0)
     const logs = []
-
-    signals3.effect(() => {
+    const unsub = signals3.effect(() => {
       logs.push($store())
     })
 
     $store($store() + 1)
     $store($store() + 1)
 
-    console.assert(logs.length === 3)
+    unsub()
+    assert.equal(logs.length, 3)
   })
   .add('agera / effect', () => {
     const $store = agera.signal(0)
     const logs = []
-
-    agera.effect(() => {
+    const unsub = agera.effect(() => {
       logs.push($store())
     })
 
     $store($store() + 1)
     $store($store() + 1)
 
-    console.assert(logs.length === 3)
+    unsub()
+    assert.equal(logs.length, 3)
+  })
+  .add('agera / effect (update fn)', () => {
+    const $store = agera.signal(0)
+    const logs = []
+    const unsub = agera.effect(() => {
+      logs.push($store())
+    })
+
+    $store(value => value + 1)
+    $store(value => value + 1)
+
+    unsub()
+    assert.equal(logs.length, 3)
   })
   .add('agera / effect + mountable', () => {
     const $store = agera.mountable(agera.signal(0))
     const logs = []
-
-    agera.effect(() => {
+    const unsub = agera.effect(() => {
       logs.push($store())
     })
 
     $store($store() + 1)
     $store($store() + 1)
 
-    console.assert(logs.length === 3)
+    unsub()
+    assert.equal(logs.length, 3)
+  })
+  .add('rxjs / BehaviorSubject (effect)', () => {
+    const $store = new rxjs.BehaviorSubject(0)
+    const logs = []
+    const sub = $store.subscribe(v => logs.push(v))
+
+    $store.next($store.getValue() + 1)
+    $store.next($store.getValue() + 1)
+
+    sub.unsubscribe()
+    assert.equal(logs.length, 3)
+  })
+  .add('effector / watch (effect)', () => {
+    const inc = effector.createEvent()
+    const $store = effector.createStore(0).on(inc, s => s + 1)
+    const logs = []
+    const unsub = $store.watch(v => logs.push(v))
+
+    inc()
+    inc()
+
+    unsub()
+    assert.equal(logs.length, 3)
+  })
+  .add('mobx / autorun (effect)', () => {
+    const $store = mobx.observable.box(0)
+    const logs = []
+    const disposer = mobx.autorun(() => {
+      logs.push($store.get())
+    })
+    const inc = mobx.action(() => {
+      $store.set($store.get() + 1)
+    })
+
+    inc()
+    inc()
+
+    disposer()
+    assert.equal(logs.length, 3)
+  })
+  .add('valtio / subscribe (effect)', async () => {
+    const $store = valtio.proxy({
+      v: 0
+    })
+    const logs = [$store.v]
+    const unsub = valtio.subscribe($store, () => logs.push($store.v))
+
+    $store.v += 1
+    await Promise.resolve()
+    $store.v += 1
+    await Promise.resolve()
+
+    unsub()
+    assert.equal(logs.length, 3)
+  })
+  .add('svelte/store / subscribe (effect)', () => {
+    const $store = svelteStore.writable(0)
+    const logs = []
+    const unsub = $store.subscribe(v => logs.push(v))
+
+    $store.update(n => n + 1)
+    $store.update(n => n + 1)
+
+    unsub()
+    assert.equal(logs.length, 3)
+  })
+  .add('jotai / createStore (sub)', () => {
+    const store = jotai.createStore()
+    const a = jotai.atom(0)
+    const logs = [store.get(a)]
+    const unsub = store.sub(a, value => logs.push(value))
+
+    store.set(a, c => c + 1)
+    store.set(a, c => c + 1)
+
+    unsub()
+    assert.equal(logs.length, 3)
   })
 
 await bench.run()
